318177d469c85cabd763662c1856c0c85c0aa4c5
xujinjun <xujinjun@tuia.cn>
2019-05-23 17:37:05 +0800

    feat: enum 枚举类型

--- greeter.js
+++ greeter.js
@@ -12,3 +12,17 @@ function greeter(person) {
 }
 var user = new Student("Jane", "M.", "User");
 document.body.innerHTML = greeter(user);
+var isLoading = false;
+var a = ['1', '2'];
+var x;
+x = [1, 'sdf', false];
+console.log(x[1].substr(1, 2));
+var abc;
+(function (abc) {
+    abc[abc["red"] = 1] = "red";
+    abc[abc["green"] = 2] = "green";
+    abc[abc["black"] = 3] = "black";
+})(abc || (abc = {}));
+var b = abc.red;
+var c = abc[2];
+console.log(c);
--- greeter.ts
+++ greeter.ts
@@ -17,3 +17,18 @@ function greeter(person : Person) {
 let user = new Student("Jane", "M.", "User");
 
 document.body.innerHTML = greeter(user);
+
+
+let isLoading: boolean = false
+let a: Array<string> = ['1', '2']
+
+let x: [number, string, boolean]
+x = [1, 'sdf', false]
+console.log(x[1].substr(1, 2))
+
+enum abc {red = 1, green = 2, black = 3}
+let b: abc = abc.red
+
+let c = abc[2]
+console.log(c)
+
